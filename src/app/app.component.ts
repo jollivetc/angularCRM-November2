9666a554aa6e31ff03090511e1af5e6661dfe0b2
Christophe Jollivet <jollivet@apside.fr>
2022-11-21 17:52:38 +0100

    binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularCRM';
+  price = 10;
+  imageUrl = 'favicon.ico'
+  legend= 'nice favicon'
+  niceClass = 'backInRed'
+  counter = 0;
+  fruits = ['apple', 'banana','orange','strawberry']
+
+  increaseCounter():void{
+    this.counter++
+  }
 }
