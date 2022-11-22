282e1a030bea0aa700f704bc23eb413fad63dec6
Christophe Jollivet <jollivet@apside.fr>
2022-11-22 15:22:42 +0100

    forms

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'crm-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  loginForm:FormGroup;
+  constructor() {
+    this.loginForm = new FormGroup({
+      login: new FormControl('',[Validators.required, Validators.minLength(3)]),
+      password: new FormControl('',[Validators.required, no$InPassword])
+    })
+  }
+
+  ngOnInit(): void {
+  }
+
+  logMeIn():void{
+    console.log(this.loginForm);
+  }
+}
+function no$InPassword(c:AbstractControl):ValidationErrors|null {
+  if((c.value as string).indexOf('$')<0){
+    return null
+  }else {
+    return {'no$InPassword': 'no $ in password'};
+  }
+}
