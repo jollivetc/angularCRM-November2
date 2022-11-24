23e6c2dc0beb2208d3999da4b60a2c17b1695490
Christophe Jollivet <jollivet@apside.fr>
2022-11-24 15:27:35 +0100

    list of consumers

---
+++ src/app/consumer/consumer-fiche/consumer-fiche.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'crm-consumer-fiche',
+  templateUrl: './consumer-fiche.component.html',
+  styleUrls: ['./consumer-fiche.component.scss']
+})
+export class ConsumerFicheComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
